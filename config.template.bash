9fb9c945c5b3c9fd8147fff00eb637d719a57aa7
Paul Stodghill <paul.stodghill@usda.gov>
2023-11-20 10:32:17 -0500

    Added REPLICON_NAMES, SKIP_FASTTREE, and SKIP_PA.

--- config.template.bash
+++ config.template.bash
@@ -12,9 +12,19 @@ GENOMES_EXT=.gff+fna
 # OUTGROUPS+=" Name1"
 # OUTGROUPS+=" Name2"
 
+# # a tab-delimed file for replicon name substitutions (*cough* Prokka *cough*)
+# # - column 1: replicon name as it appears in input genomes
+# # - column 2: replicon name as it should appear in the output
+# REPLICON_NAMES=local/names.txt
+
 # ------------------------------------------------------------------------
 
 # ROARY_ARGS="-g 60000" # increase the max number of clusters from 50000.
+
+# ------------------------------------------------------------------------
+
+# SKIP_FASTTREE=1 # don't run FastTreeMP
+# SKIP_PA=1 # don't run presence/absence analysis
 
 # ------------------------------------------------------------------------
 
